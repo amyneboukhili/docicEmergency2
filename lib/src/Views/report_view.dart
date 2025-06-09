35bab8f606f1c76545745a5f9c57bea9d750043d
inesachour <ines.achour@insat.ucar.tn>
2025-06-09 17:02:05 +0100

    health infos

--- lib/src/Views/report_view.dart
+++ lib/src/Views/report_view.dart
@@ -21,8 +21,26 @@ class ReportView extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                healthInfoWidget(color: Color(0xFFDFC5CF), icon: Icon(Icons.pin_drop), title: "Blood Type", value: "A+"),
-                healthInfoWidget(color: Color(0xFFFBF0DC), icon: Icon(Icons.line_weight), title: "Weight", value: "59KG"),
+                healthInfoWidget(
+                  color: Color(0xFFDFC5CF),
+                  icon: Icon(
+                    Icons.bloodtype,
+                    color: Color(0xFF9C4A6B),
+                    size: 40.r,
+                  ),
+                  title: "Blood Type",
+                  value: "A+",
+                ),
+                healthInfoWidget(
+                  color: Color(0xFFFBF0DC),
+                  icon: Icon(
+                    Icons.monitor_weight,
+                    color: Color(0xFFD2B47D),
+                    size: 40.r,
+                  ),
+                  title: "Weight",
+                  value: "59KG",
+                ),
               ],
             ),
           ],
@@ -38,13 +56,28 @@ class ReportView extends StatelessWidget {
     required Icon icon,
   }) {
     return Container(
-      height: 150.w,
+      height: 145.w,
       width: 150.w,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(20),
       ),
-      child: Column(children: [icon, Text(title), Text(value)]),
+      padding: EdgeInsets.symmetric(horizontal: 18.w, vertical: 6.h),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          icon,
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+          ),
+          Text(
+            value,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+          ),
+        ],
+      ),
     );
   }
 }
